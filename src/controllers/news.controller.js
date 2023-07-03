75f30b18c6dd2f846e43be9d2d791306dea4b249
Renan Cesar <cesar.renan@hotmail.com>
2023-07-02 22:45:16 -0300

    feat: Adicionando paginação em News

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {    
@@ -24,14 +24,57 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
+  
   try {
-    const news = await findAllService();
+    let {limit, offset} = req.query;
+  
+    limit = Number(limit);
+    offset = Number(offset);
+  
+    if (!limit) {
+      limit = 5;
+    }
+  
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+ 
+
+    const next = offset + limit;
+    const nextUrl = next < total? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previuos = offset - limit < 0? null : offset - limit;
+    const previuosUrl =  previuos != null ? `${currentUrl}?limit=${limit}&offset=${previuos}`: null;
+
 
     if (news.length === 0) {
       return res.status(400).send({ message: "Nenhuma postagem registrada!" });
     }
 
-    res.status(200).send(news);
+    res.status(200).send({
+      nextUrl,
+      previuosUrl,
+      limit,
+      offset,
+      total,
+
+      results: news.map( item => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        userName: item.user.username,
+        img : item.user.img,
+      }))
+
+    });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,6 +1,7 @@
 import News from "../models/News.js";
 
 const createService = (body) => News.create(body);
-const findAllService = () => News.find();
+const findAllService = (offset, limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
+const countNews = () => News.countDocuments();
 
-export { createService, findAllService };
+export { createService, findAllService, countNews };
