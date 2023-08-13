7b746c3820627f77e45f99433ec1f919a9e87714
Renan Cesar <cesar.renan@hotmail.com>
2023-08-13 19:53:55 -0300

    refac: Alterando retorno do token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const login = async (req, res) => {
     return res
       .status(200)
       .header("Authorization", "Bearer " + token)
-      .send({ message: "Login realizado com sucesso!", token: token });
+      .send({ message: "Login realizado com sucesso!", token: `Bearer ${token}` });
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
